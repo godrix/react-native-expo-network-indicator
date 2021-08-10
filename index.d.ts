f1be5662f134768d1e83485854e8802c8a7c1339
Carlos Gabriel Godri <08508575971@itajai.local>
2021-08-10 14:04:34 -0300

    [fix] position absolute

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,8 @@ interface NetworkStatusProps{
   message?:string;
   color?:string;
   colorText?:string;
-  checkTime?:number;
+  icon?:'network-strength-off'|'close-network-outline'|'cloud-alert'|'alert-circle'|'alert';
+  children:React.ReactNode;
 }
  interface useNetworkAsyncProps{
   ipAddress?:string;
@@ -27,5 +28,5 @@ declare enum NetworkStateType {
   OTHER = "OTHER"
 }
 
-export function NetworkStatus({message, color, colorText, checkTime}:NetworkStatusProps):React.JSXElementConstructor;
+export function NetworkStatus({message, color, colorText, children}:NetworkStatusProps):React.JSXElementConstructor;
 export function useNetworkAsync():Promise<useNetworkAsyncProps>;
